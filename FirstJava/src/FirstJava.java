40c2c08bc59aaedc5beed02a42d345cb51c7555c
bhushanGap91 <bhushangap@gmail.com>
2016-10-07 14:07:00 +0530

    change in FirstJava file

--- FirstJava/src/FirstJava.java
+++ FirstJava/src/FirstJava.java
@@ -3,7 +3,7 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        //print output  
+          
 		System.out.println("Hello git");
 	}
 
